dfa114c4a23239d99279de239f66d7c5dfa32abf
Jake Simpson <jsimo42@gmail.com>
2020-03-05 21:47:21 +0000

    V1.28 -Added Mem Leak Detection

--- TerrainGenerator_FYP/TerrainGenerator_FYP/main.cpp
+++ TerrainGenerator_FYP/TerrainGenerator_FYP/main.cpp
@@ -3,6 +3,12 @@
 
 #include "WorldEngine.h"
 
+//Check for memory leaks
+#if defined(DEBUG) | defined(_DEBUG)
+#include <crtdbg.h>
+#define new new(_NORMAL_BLOCK,__FILE__, __LINE__)
+#endif
+
 int main()
 {
 	WorldEngine* Engine = new WorldEngine;
